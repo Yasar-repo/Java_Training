fe066d3b0090461fe50f6b8f8d1a3e6f9a5793c5
Yasar <yasarshariff20@gmail.com>
2021-07-10 08:21:16 +0530

    First commit

---
+++ Java_Training/src/day1/Basic.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class Basic {
+public static void main(String[] args) {
+	char cc='a';
+	String str="hello";
+	double d=2.45;//64 bit
+	float f=2.45f;//32 bit
+	int i=100;
+	int j=200;
+	int sum=i+j;
+	System.out.println("The sum is :"+sum);
+	System.out.println(cc);
+	System.out.println(str);
+	System.out.println(d);
+	System.out.println(f);
+}
+}
